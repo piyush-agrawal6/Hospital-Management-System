0b924b8510de269a4892c677ed366f23d6c25b1b
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-12-21 15:56:46 +0530

    profile  page addded

--- src/Routes/AllRoutes.jsx
+++ src/Routes/AllRoutes.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Footer from "../Components/Footer/Footer";
 import Navbar from "../Components/Navbar/Navbar";
+import Dashboard from "../Pages/Dashboard/Dashboard";
 import Home from "../Pages/Home/Home";
 const AllRoutes = () => {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Dashboard />}></Route>
       </Routes>
       {/* <Footer /> */}
     </>
