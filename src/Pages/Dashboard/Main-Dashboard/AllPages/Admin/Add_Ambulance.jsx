502704a09bc003adade190fc569b43bfe72b0a0f
Rajendra <itsrajendrapatel@gmail.com>
2023-01-20 12:18:58 +0530

    Today's work Done

--- src/Pages/Dashboard/Main-Dashboard/AllPages/Admin/Add_Ambulance.jsx
+++ src/Pages/Dashboard/Main-Dashboard/AllPages/Admin/Add_Ambulance.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import one from "../../../../../img/ambuone.png";
+import two from "../../../../../img/ambutwo.png";
+import three from "../../../../../img/ambuthree.png";
+import "./CSS/Add_Ambu.css";
 
 const Add_Ambulance = () => {
-  return <div>Add_Ambulance</div>;
+  let [ambuType, setambuType] = useState("");
+  return (
+    <>
+      <div className="mainAmbupance">
+        <h1>Add Ambulance</h1>
+        <div className="imagesection">
+          <img
+            src={one}
+            alt="first_img"
+            onClick={() => setambuType("Mobile ICU Ambulance")}
+          />
+          <img
+            src={two}
+            alt="first_img"
+            onClick={() => setambuType("Basic Life Support Ambulance")}
+          />
+          <img
+            src={three}
+            alt="first_img"
+            onClick={() => setambuType("Collective Ambulance")}
+          />
+        </div>
+        {/* ******************************************************** */}
+        <form>
+          <div>
+            <label>Ambulance Type</label>
+            <div className="inputdiv">
+              <input type="text" placeholder="Select img" value={ambuType} />
+            </div>
+          </div>
+          <div>
+            <label>Price per Hours</label>
+            <div className="inputdiv">
+              <input type="text" placeholder="eg.200/-" />
+            </div>
+          </div>
+          <div>
+            <label>Ambulance Code</label>
+            <div className="inputdiv">
+              <input type="number" placeholder="eg.EVB36A" />
+            </div>
+          </div>
+          <div>
+            <label>Driver Name</label>
+            <div className="inputdiv">
+              <input type="number" placeholder="Name" />
+            </div>
+          </div>
+          <div>
+            <label>Driver Contact No</label>
+            <div className="inputdiv">
+              <input type="number" placeholder="Contact No" />
+            </div>
+          </div>
+          <button type="submit" className="formsubmitbutton">
+            Submit
+          </button>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default Add_Ambulance;
--- src/Pages/Dashboard/Main-Dashboard/AllPages/Admin/Add_Doctor.jsx
+++ src/Pages/Dashboard/Main-Dashboard/AllPages/Admin/Add_Doctor.jsx
@@ -7,49 +7,100 @@ const AddDoctor = () => {
       <h1>Add Doctors</h1>
       <form>
         <div>
-          <p>Doctor Name</p>
-          <div>
+          <label>Doctor Name</label>
+          <div className="inputdiv">
             <input type="text" placeholder="First Name" />
             <input type="text" placeholder="Last Name" />
           </div>
         </div>
         <div>
-          <p>Birthdate</p>
-          <input type="date" placeholder="dd-mm-yy" />
+          <label>Birthdate</label>
+          <div className="inputdiv">
+            <input type="date" placeholder="dd-mm-yy" />
+          </div>
         </div>
         <div>
-          <p>Aadhar Card No.</p>
-          <input type="number" placeholder="Aadhar Card No" />
+          <label>Aadhar Card No.</label>
+          <div className="inputdiv">
+            <input type="number" placeholder="Aadhar Card No" />
+          </div>
         </div>
         <div>
-          <p>Emergency Contact Number</p>
-          <input type="number" placeholder="Emergency Number" />
+          <label>Emergency Contact Number</label>
+          <div className="inputdiv">
+            <input type="number" placeholder="Emergency Number" />
+          </div>
         </div>
         <div>
-          <p>Email</p>
-          <input type="number" placeholder="abc@abc.com" />
+          <label>Email</label>
+          <div className="inputdiv">
+            <input type="number" placeholder="abc@abc.com" />
+          </div>
         </div>
         <div>
-          <p>Blood Group</p>
-          <select>
-            <option value="Choose Blood Group">Choose Blood Group</option>
-            <option value="A+">A+</option>
-            <option value="A-">A-</option>
-            <option value="B+">B+</option>
-            <option value="B-">B-</option>
-            <option value="AB+">AB+</option>
-            <option value="AB-">AB-</option>
-            <option value="O+">O+</option>
-            <option value="O-">O-</option>
-          </select>
+          <label>Blood Group</label>
+          <div className="inputdiv">
+            <select>
+              <option value="Choose Blood Group">Select</option>
+              <option value="A+">A+</option>
+              <option value="A-">A-</option>
+              <option value="B+">B+</option>
+              <option value="B-">B-</option>
+              <option value="AB+">AB+</option>
+              <option value="AB-">AB-</option>
+              <option value="O+">O+</option>
+              <option value="O-">O-</option>
+            </select>
+          </div>
         </div>
         <div>
-          <p>Address</p>
-          <div>
+          <label>Address</label>
+          <div className="inputdiv">
             <input type="text" placeholder="Address line 1" />
             <input type="text" placeholder="Address line 2" />
           </div>
         </div>
+        <div>
+          <label>Education</label>
+          <div className="inputdiv">
+            <input type="text" placeholder="eg.MBBS" />
+          </div>
+        </div>
+        <div>
+          <label>Specility</label>
+          <div className="inputdiv">
+            <input type="text" placeholder="Specility" />
+          </div>
+        </div>
+        <div>
+          <label>Hospital Name</label>
+          <div className="inputdiv">
+            <input type="text" placeholder="e.g : City Hospital" />
+          </div>
+        </div>
+        <div>
+          <label>Hospital Address</label>
+          <div className="inputdiv">
+            <input type="text" placeholder="building/area" />
+            <input type="text" placeholder="city" />
+            <input type="text" placeholder="state" />
+          </div>
+        </div>
+        <div>
+          <label>Doctor ID</label>
+          <div className="inputdiv">
+            <input type="text" placeholder="ID" />
+          </div>
+        </div>
+        <div>
+          <label>Password ID</label>
+          <div className="inputdiv">
+            <input type="text" placeholder="Password" />
+          </div>
+        </div>
+        <button type="submit" className="formsubmitbutton">
+          Submit
+        </button>
       </form>
     </div>
   );
